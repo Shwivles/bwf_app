b6aa5c9dfff5a795ad758e95360e804d05ca41aa
Nick <nick@Nicholass-MacBook-Pro.local>
2020-09-15 05:16:27 -0700

    add functions to handle timer and add more options for navbar

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -3,6 +3,7 @@ import React from 'react';
 //bootstrap
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navi() {
    
@@ -15,7 +16,14 @@ function Navi() {
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#about">About</Nav.Link>
+                    <NavDropdown title="Workouts" id="collasible-nav-dropdown">
+                        <NavDropdown.Item>Full Body</NavDropdown.Item>
+                        <NavDropdown.Item>Push</NavDropdown.Item>
+                        <NavDropdown.Item>Pull</NavDropdown.Item>
+                        <NavDropdown.Item>Legs</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
+                
             </Navbar.Collapse>
 
         </Navbar>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { timerStart, timerStop, reset, updateSec, updateMin } from '../actions';
+//import tickReducer from '../reducers/time_tick';
 
 function Timer() {
 
     const time = useSelector(state => state.tickReducer);
+    const dispatch = useDispatch();
 
-    const handleTime = () => {
+    const tick = () => {
 
     }
+
+    const handleTime = () => {
+        if (time.onOff === 'off') {
+            dispatch(timerStart());
+        } else {
+            dispatch(timerStop());
+        }
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (time.onOff === 'off') {
+                tick();
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    });
 
     return (
         <div>
             <div>
                 {time.minutes}:{time.seconds}
+            </div>
+            <div>
+                <button>{time.ssButton}</button>
+                <button>Reset</button>
             </div>
         </div>
     );
