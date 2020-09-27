cf336dd98a15aae1adda1f66a051cfea649c3259
Nick <lee.nick44@gmail.com>
2020-09-27 09:24:04 -0700

    add write-up for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navi from '../components/Navi';
+import { Link } from 'react-router-dom';
 
 //bootstrap
 import Container from 'react-bootstrap/Container';
@@ -8,11 +9,19 @@ function About() {
     return (
         <div>
             <Navi />
-            <Container>
+            <Container className="about-container">
                 <h2>About the Site</h2>
                 <p>
-                    This website was made as another workout tracker for the recomended routine from Reddit's bodyweight fitness subreddit.
+                    This website was made as workout tracker for the recomended routine from Reddit's bodyweight fitness subreddit.
                     All info involving the workout can be found on Reddit's bodyweightfitness subreddit. 
+                </p>
+                <p>
+                    <Link target="_blank" to={"//www.reddit.com/r/bodyweightfitness/wiki/index"}>Link to the subreddit wiki</Link>
+                </p>
+                <p>
+                    This is purely a personal project to see if I could recreate a tracker similar to others that have been shown 
+                    on the subreddit. The site features a all-in-one timer that can be used for breaks and timed exercises like planks.
+                    Each section of the workout also has a drobdown menu to select a progression of an exercise.
                 </p>
             </Container>
         </div>
